c03e05e95f3fa7c757dc7255130cf1122dacceb2
Kenni <kenni.bawden@gmail.com>
2012-10-04 15:42:32 +1000

    Created a new compilation script with more options

---
+++ compile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys, os
+
+BUILD_DIR = 'build'
+
+# Check to see if given a program to compile
+if len(sys.argv) < 2:
+  print "Usage: %s filename.vpl [--ANTLR=(y|n)]" % sys.argv[0]
+  sys.exit(1)
+else:
+  if '.vpl' in sys.argv[1]:
+    prog = sys.argv[1].rstrip('.vpl')
+  else:
+    prog = None
+
+# Build the ANTLR-generated parser using the grammar file
+if '--ANTLR=n' not in sys.argv or "--ANTLR=y" in sys.argv:
+  os.system("java -cp antlr-3.1.2.jar org.antlr.Tool -o %s VPL.g" % BUILD_DIR)
+  os.system("touch %s" % os.path.join(BUILD_DIR, '__init__.py'))
+
+# Use the ANTLR-generated parser to convert the VPL program to ASM
+if prog:
+  os.system("./vpl2asm.py < %(prog)s.vpl > %(prog)s.vpl.s" % {'prog': prog})
+
+  # Compile the ASM and C file together
+  if os.path.isfile(prog+'.c'):
+    # Use prog.c
+    os.system("gcc -Wall -W %(prog)s.c %(prog)s.vpl.s -o %(prog)s" % {'prog': prog})
+  else:
+    # Use main.c
+    os.system("gcc -Wall -W main.c %(prog)s.vpl.s -o %(prog)s" % {'prog': prog})
+
