a92cd4f0f16a270ae7817984c7bb97c2af3c98ff
Kenni <kenni.bawden@gmail.com>
2012-10-09 16:34:39 +1100

    C files can be compiled without assembly

--- compile.py
+++ compile.py
@@ -10,23 +10,39 @@ if len(sys.argv) < 2:
 else:
   if '.vpl' in sys.argv[1]:
     prog = sys.argv[1].rstrip('.vpl')
+    ext = "vpl"
+  elif '.c' in sys.argv[1]:
+    prog = sys.argv[1].rstrip('.c')
+    ext = "c"
   else:
     prog = None
+    ext = None
 
 # Build the ANTLR-generated parser using the grammar file
-if '--ANTLR=n' not in sys.argv or "--ANTLR=y" in sys.argv:
+if '--ANTLR=n' not in sys.argv or "--ANTLR=y" in sys.argv and ext!="c":
   os.system("java -cp antlr-3.1.2.jar org.antlr.Tool -o %s VPL.g" % BUILD_DIR)
   os.system("touch %s" % os.path.join(BUILD_DIR, '__init__.py'))
 
 # Use the ANTLR-generated parser to convert the VPL program to ASM
 if prog:
-  os.system("./vpl2asm.py < %(prog)s.vpl > %(prog)s.vpl.s" % {'prog': prog})
+  if ext == "vpl":
+    os.system("./vpl2asm.py < %(prog)s.vpl > %(prog)s.vpl.s" % {'prog': prog})
+  
+  # Compile options
+  call = "gcc -Wall -W -I vector vector/vector.c -o %(prog)s "
 
-  # Compile the ASM and C file together
+  # Choose C file
   if os.path.isfile(prog+'.c'):
     # Use prog.c
-    os.system("gcc -Wall -W -I vector %(prog)s.c %(prog)s.vpl.s vector/vector.c -o %(prog)s" % {'prog': prog})
+    call += "%(prog)s.c "
   else:
     # Use main.c
-    os.system("gcc -Wall -W -I vector main.c %(prog)s.vpl.s vector/vector.c -o %(prog)s" % {'prog': prog})
+    call += "main.c "
+
+  # Include assembly
+  if ext == "vpl":
+    call += "%(prog)s.vpl.s"
+
+  # Compile
+  os.system(call % {'prog': prog})
 
