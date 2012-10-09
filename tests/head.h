2bc11d59d611c31b8c4f6042626de343513356c1
Elizaveta Fedorenko <lisafedorenko@gmail.com>
2012-10-09 19:47:19 +1100

    made a header file and removed header parts from c files

---
+++ tests/head.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+// alignment macro: aligns a memory block a to multiples of a
+#define align(s,a) (((size_t)(s) + ((a)-1)) & ~ ((size_t) (a) - 1))
+// alignment for SSE unit
+#define SSE_ALIGN (16)
+// number of elements
+#define NUM (4)
