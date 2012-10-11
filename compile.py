3ce3233e4b23b81ad6892e9495ae465d851845fb
Kenni <kenni.bawden@gmail.com>
2012-10-11 21:08:54 +1100

    Compiler always includes asembly if available

--- compile.py
+++ compile.py
@@ -3,9 +3,11 @@ import sys, os
 
 BUILD_DIR = 'build'
 
+usage = "Usage: %s filename.[vpl|c] [--ANTLR=(y|n)]" % sys.argv[0]
+
 # Check to see if given a program to compile
 if len(sys.argv) < 2:
-  print "Usage: %s filename.vpl [--ANTLR=(y|n)]" % sys.argv[0]
+  print usage
   sys.exit(1)
 else:
   if '.vpl' in sys.argv[1]:
@@ -14,9 +16,12 @@ else:
   elif '.c' in sys.argv[1]:
     prog = sys.argv[1].rstrip('.c')
     ext = "c"
-  else:
+  elif "--ANTLR" in sys.argv[1]:
     prog = None
     ext = None
+  else:
+    print usage
+    sys.exit(1)
 
 # Build the ANTLR-generated parser using the grammar file
 if '--ANTLR=n' not in sys.argv or "--ANTLR=y" in sys.argv and ext!="c":
@@ -29,7 +34,7 @@ if prog:
     os.system("./vpl2asm.py < %(prog)s.vpl > %(prog)s.vpl.s" % {'prog': prog})
   
   # Compile options
-  call = "gcc -Wall -W -I vector vector/vector.c -o %(prog)s "
+  call = "gcc -g -Wall -W -I vector vector/vector.c -o %(prog)s "
 
   # Choose C file
   if os.path.isfile(prog+'.c'):
@@ -40,7 +45,7 @@ if prog:
     call += "main.c "
 
   # Include assembly
-  if ext == "vpl":
+  if os.path.isfile(prog+'.vpl.s'):
     call += "%(prog)s.vpl.s"
 
   # Compile
