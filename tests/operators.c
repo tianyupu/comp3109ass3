e9ceabdaa8b05057353f637cc08c620556632d8f
Kenni <kenni.bawden@gmail.com>
2012-10-11 22:27:50 +1100

    Fixed operators tests

--- tests/operators.c
+++ tests/operators.c
@@ -9,16 +9,18 @@ int main(void) {
         *b = createvect(NUM);
 
   // test 0
-  test0(NUM, a);
   setvect(NUM, a, 1);
+  test0(NUM, a);
+
   printf("0: a = ");
   printvect(NUM, a);
   printf("\n");
   
   // test 1
-  test1(NUM, a, b);
   setvect(NUM, a, 1);
   setvect(NUM, b, 0);
+  test1(NUM, a, b);
+
   printf("1: a = ");
   printvect(NUM, a);
   printf("\n");
@@ -27,8 +29,9 @@ int main(void) {
   printf("\n");
   
   // test 2
-  test2(NUM, a);
   setvect(NUM, a, 1);
+  test2(NUM, a);
+
   printf("2: a = ");
   printvect(NUM, a);
   printf("\n");
