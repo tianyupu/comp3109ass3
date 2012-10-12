6eabbfc0db6dcf0b84358167f64175332c5337e0
Kenni <kenni.bawden@gmail.com>
2012-10-12 16:04:17 +1100

    Trees print out neatly

--- VPL/base.py
+++ VPL/base.py
@@ -5,15 +5,31 @@ class Base():
   def header(self):
     return HEADER_ASM.format(
       name = self.ast_node.text+" : line "+str(self.ast_node.line),
-      tree = self.ast_node.toStringTree(),
+      tree = self.format_tree(self.ast_node.toStringTree()),
     )
+
+  def format_tree(self, tree):
+    new_tree = ""
+    level = 0
+    for c in tree:
+      if c == "(":
+        level += 1
+        new_tree.rstrip()
+        new_tree += "\n    #"+"\t"*level
+
+      elif c == ")":
+        level -= 1
+      
+      new_tree += c
+    
+    return new_tree
 
 
 HEADER_ASM = """
     ###########################################################################
     #
     #{name:^75}
-    #{tree:^75}
+    #{tree}
     #
     ###########################################################################
 """
